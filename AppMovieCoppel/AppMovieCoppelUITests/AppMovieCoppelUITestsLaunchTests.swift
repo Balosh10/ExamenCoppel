8b8f9346e0682b066b455e0268d3e4e81f1a1635
OsvaldoCH <osvaldo.cespedes@datasystems.com.mx>
2023-02-22 17:17:45 -0600

    create a movie app project for coppel exam

---
+++ AppMovieCoppel/AppMovieCoppelUITests/AppMovieCoppelUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  AppMovieCoppelUITestsLaunchTests.swift
+//  AppMovieCoppelUITests
+//
+//  Created by osvaldo cespedes on 22/02/23.
+//
+
+import XCTest
+
+final class AppMovieCoppelUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
