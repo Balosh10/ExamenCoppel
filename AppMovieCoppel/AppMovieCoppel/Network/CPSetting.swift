6439840ba734fe2a7a314e637617f5e748ba4c47
OsvaldoCH <osvaldo.cespedes@datasystems.com.mx>
2023-02-25 17:56:12 -0600

    Set Up Test Environments

--- AppMovieCoppel/AppMovieCoppel/Network/CPSetting.swift
+++ AppMovieCoppel/AppMovieCoppel/Network/CPSetting.swift
@@ -11,16 +11,14 @@ struct CPSetting {
     static var accessToken = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MDA5ZDFiMTFkZWRiNWFlZTNmOWI1NjE3YjhhNDJjMiIsInN1YiI6IjVmZTEwZjZlYWI2ODQ5MDAzZjdmNzM4MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.L5FK8ilJjbClDwfCzhR8ZWbcjMRnb7xDY5m3b-LOVns"
     static var apiKey = "4009d1b11dedb5aee3f9b5617b8a42c2"
     static var imageBase = "https://image.tmdb.org/t/p/original"
+    
+    #warning("Set up test environments")
     static var baseGateway: String {
         switch Environment.init(rawValue: CPAppInfo.shared.environment){
-            case .Development:
+            case .Development, .Production, .QA:
                 return "https://api.themoviedb.org/3"
-            case .Production:
-                return ""
-            case .QA:
-                return ""
             default:
-                return ""
+                return "https://api.themoviedb.org/3"
         }
     }
     static var activateTestUser: Bool = true
